01da422a425ef1cd02c180d8f24e8cd299554778
王磊 <sysuleiw@163.com>
2019-04-16 15:15:52 +0800

    变更：fix无法编译运行的问题，最终是因为部分库同时存在于androidx和普通support库中导致
    原因：
    
    描述：无

--- app/src/main/java/com/example/androidlearningdemo/ui/activity/base/BaseActivity.java
+++ app/src/main/java/com/example/androidlearningdemo/ui/activity/base/BaseActivity.java
@@ -2,13 +2,13 @@ package com.example.androidlearningdemo.ui.activity.base;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.androidlearningdemo.ui.presenter.base.BasePresenter;
 
-import androidx.annotation.Nullable;
 import butterknife.ButterKnife;
 
 /**
